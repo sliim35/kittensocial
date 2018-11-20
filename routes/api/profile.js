de889509e317c853440c94ca0f09b0c9f8560db7
sliim35 <slim.r35@gmail.com>
2018-11-20 15:46:26 +0300

    More Profile API Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -14,6 +14,60 @@ const validateProfileInput = require('../../validation/profile');
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile works' }));
 
+// @route GET api/profile/all
+// @desc Get all profiles
+// @access Public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofiles = 'There is no profiles';
+        res.status(404).json(errors);
+      }
+      res.json(profiles);
+    })
+    .catch(() => res.status(404).json({ profiles: 'There are no profiles' }));
+});
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user id
+// @access Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(() => res.status(404).json({ profile: 'There is no profile' }));
+});
+
+// @route GET api/profile/handle/:handle
+// @desc Get profile by handle
+// @access Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(() => res.status(404).json({ profile: 'Not found' }));
+});
+
 // @route GET api/profile
 // @desc Get current users profile
 // @access Private
